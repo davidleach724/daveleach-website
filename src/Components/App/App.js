074dea2ae039c2676121896d17c26142e5605a07
David Leach <davidleach724@gmail.com>
2021-10-31 10:01:04 -0600

    Create Home component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Home from '../Home/Home'
 import './App.css'
 
 const App = () => {
 
   return (
-    <h1>Dave Leach</h1>
+    <main>
+      <Home />
+    </main>
   )
 }
 
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+
+
+  return (
+    <div className="home-container">
+      <h1>Home</h1>
+    </div>
+  )
+}
+
+export default Home
