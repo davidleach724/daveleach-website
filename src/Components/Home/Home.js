59302acfc46054985db788d8df24ee484098f46f
David Leach <davidleach724@gmail.com>
2021-11-01 15:48:49 -0600

    Add animation to name and occupation

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,7 +8,8 @@ const Home = () => {
 
   return (
     <div className="home-container">
-      <h1 className="name bounce-in-fwd">Dave Leach</h1>
+      <h1 className="name bounce-in-top">Dave Leach</h1>
+      <h2 className="occupation text-focus-in">Front End Engineer</h2>
       <img src={tree} className='image1' alt='tree'/>
       <img src={city} className='image2' alt='city'/>
       <img src={moon} className='image3' alt='moon'/>
