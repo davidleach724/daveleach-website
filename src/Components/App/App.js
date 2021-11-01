1bd4bbaf1d2bf6b4004fc49251062cf9bf8a5923
David Leach <davidleach724@gmail.com>
2021-11-01 13:39:41 -0600

    Add config for particle

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import Home from '../Home/Home'
 import './App.css'
 
 import Particles from 'react-tsparticles';
+import tsParticleConfig from '../config/tsParticleConfig';
 
 const App = () => {
   const particlesInit = (main) => {
@@ -22,78 +23,7 @@ const App = () => {
       id="tsparticles"
       init={particlesInit}
       loaded={particlesLoaded}
-      options={{
-        fpsLimit: 60,
-        interactivity: {
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 6,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }}
+      options={tsParticleConfig}
     />
       <Home />
     </main>
