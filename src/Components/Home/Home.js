0149faabef748c2970f57eb19175f62452aef20d
David Leach <davidleach724@gmail.com>
2021-11-01 18:14:05 -0600

    Add face glow

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,16 +3,18 @@ import './Home.css'
 import tree from '../../Images/Tree&Grass.png'
 import city from '../../Images/City&Grass.png'
 import moon from '../../Images/Moon2.png'
+import daveleach from '../../Images/daveleach.png'
+import softwareengineer from '../../Images/softwareengineer.png'
 
 const Home = () => {
 
   return (
     <div className="home-container">
-      <h1 className="name bounce-in-top">Dave Leach</h1>
-      <h2 className="occupation text-focus-in">Front End Engineer</h2>
-      <img src={tree} className='image1' alt='tree'/>
-      <img src={city} className='image2' alt='city'/>
-      <img src={moon} className='image3' alt='moon'/>
+      <img className="name bounce-in-top" src={ daveleach }/>
+      <img className="occupation text-focus-in" src={ softwareengineer }/>
+      <img src={ tree } className='image1' alt='tree'/>
+      <img src={ city } className='image2' alt='city'/>
+      <img src={ moon } className='image3' alt='moon'/>
     </div>
   )
 }
