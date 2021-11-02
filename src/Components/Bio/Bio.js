bb3366ab9acf47af89dd95907e76876326cacd7e
David Leach <davidleach724@gmail.com>
2021-11-02 14:21:28 -0600

    Add text sections

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -6,6 +6,15 @@ const Bio = () => {
     <div className="bio-container">
       <div className='pagebreak'></div>
       <h1 className='section-title'>Bio Goes Here</h1>
+      <article className="article-container">
+        <div className="left-article">
+           <p>Yr tacos blog, locavore chartreuse tote bag gochujang kogi celiac. Leggings flexitarian mixtape, craft beer portland chillwave coloring book 90's vinyl jianbing godard locavore. Gentrify celiac food truck locavore yuccie biodiesel ugh umami. Put a bird on it snackwave food truck yr. Fixie artisan franzen chia butcher. Try-hard glossier tofu snackwave wolf yr man bun.</p>
+        </div>
+        <div className="right-article">
+          <p>Migas literally single-origin coffee cornhole brooklyn hammock. Retro iceland leggings hashtag. Sartorial meggings before they sold out twee vaporware celiac post-ironic man braid taxidermy hexagon palo santo. Occupy yuccie meditation street art.</p>
+          <p>Dummy text? More like dummy thicc text, amirite?</p>
+        </div>
+      </article>
     </div>
   )
 }
