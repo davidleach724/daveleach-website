ee84d1d64270deb69739cb723be9e12d2906ce42
David Leach <davidleach724@gmail.com>
2021-11-02 17:40:16 -0600

    Bio content

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -5,14 +5,16 @@ const Bio = () => {
   return (
     <div className="bio-container">
       <div className='pagebreak'></div>
-      <h1 className='section-title'>Bio Goes Here</h1>
+      <h1 className='section-title'>About Me</h1>
       <article className="article-container">
         <div className="indiv-article">
-           <p>Yr tacos blog, locavore chartreuse tote bag gochujang kogi celiac. Leggings flexitarian mixtape, craft beer portland chillwave coloring book 90's vinyl jianbing godard locavore. Gentrify celiac food truck locavore yuccie biodiesel ugh umami. Put a bird on it snackwave food truck yr. Fixie artisan franzen chia butcher. Try-hard glossier tofu snackwave wolf yr man bun.</p>
+           <p>I spent the past 10 years playing music, touring around the world, and performing on notable programs such as "The Late Show with David Letterman." When the world shut down in March of 2020, I was able to catch my breath and had the opportunity to plan for what came next. In May of 2021 I enrolled in Turing School of Software & Design and discovered just how similar the programming and musical brains are.</p>
+           <p>Like music, you're constantly presented with opportunities to learn something new. The frustrating parallel is you'll never come close to learning it all; the positive parallel is you will only become stale when you decide to. There is always more to learn.</p>
         </div>
         <div className="indiv-article">
-          <p>Migas literally single-origin coffee cornhole brooklyn hammock. Retro iceland leggings hashtag. Sartorial meggings before they sold out twee vaporware celiac post-ironic man braid taxidermy hexagon palo santo. Occupy yuccie meditation street art.</p>
-          <p>Dummy text? More like dummy thicc text, amirite?</p>
+          <p>My past experience in music and as an educator has provided me excellent problem-solving, time management, and communication skills that result in a quality addition to a team. I spent most of my life explaining concepts in anologies becuase what I was covering wasn't tangible. I received a Bachelors in Business Administration in  Management Information Systems from the University of Oklahoma that gave me a broad overview of how technology and databases function within businesses. I believe that curiosity, drive, and passion are an excellent and almost dangerous combination for an emerging developer - I'm excited to see what happens.
+          </p>
+          <p>Feel free to reach out to me on linkedIn, email me directly, or take a look at my resume.</p>
         </div>
       </article>
     </div>
