de105c9ada9af3c821190ac251d10a9157e6f8f1
David Leach <davidleach724@gmail.com>
2021-11-03 19:25:25 -0600

    Add default squares for projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -5,6 +5,14 @@ const Projects = () => {
   return (
     <div className="projects-container">
       <h2 className="skills-title">Projects</h2>
+      <div className="project-card-container">
+        <div className="project- project-card"></div>
+        <div className="project- project-card"></div>
+        <div className="project- project-card"></div>
+        <div className="project- project-card"></div>
+        <div className="project- project-card"></div>
+        <div className="project- project-card"></div>
+      </div>
     </div>
   )
 }
