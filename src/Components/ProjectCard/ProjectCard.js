07af295ced3503af95f9393f275b02439bbe25e4
David Leach <davidleach724@gmail.com>
2021-11-06 12:46:36 -0600

    Add hover layer

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -4,13 +4,16 @@ const ProjectCard = ({ project }) => {
 
   return (
     <div className="project-card">
-      <h3>{project.name}</h3>
       <img className="project-image" src={
           process.env.PUBLIC_URL +
           "/projectimages/" +
           project.image
         }
         />
+        <div className="project-layer">
+          <h3>{project.name}</h3>
+          <h5>{project.summary}</h5>
+        </div>
     </div>
   )
 }
