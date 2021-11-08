a5be96eec3a9501234d6fb483c1f0ae1c89360e1
David Leach <davidleach724@gmail.com>
2021-11-07 17:38:58 -0700

    Edit bio

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -13,11 +13,11 @@ const Bio = () => {
       <h2 className='section-title'>About Me</h2>
       <article className="article-container">
         <div className="indiv-article">
-           <p>I spent the past 10 years touring around the world with several artists playing music. When everything came to a halt in March of 2020, I was able to catch my breath and plan my next move. In May of 2021 I enrolled in Turing School of Software & Design and discovered just how similar the programming and musical brains are.</p>
+           <p>I spent the past 10 years traveling around the world perfoming music with several different artists. When everything came to a halt in March of 2020, I was forced to catch my breath and plan my next move. In May of 2021 I enrolled in Turing School of Software & Design and discovered just how similar the programming and musical brains are.</p>
            <p>Like music, you're constantly presented with opportunities to learn something new. The frustrating parallel is you'll never come close to learning it all; the positive parallel is you will only become stale when you decide to. There is always more to learn.</p>
         </div>
         <div className="indiv-article">
-          <p>My past experience  as an artist, educator, and entrepreneur has provided me excellent problem-solving, time management, and communication skills. I spent most of my life explaining concepts in anologies becuase what I was covering wasn't tangible. I believe that curiosity, drive, and passion are an excellent and almost dangerous combination for an emerging developer - I'm excited to see what happens.
+          <p>My past experience as an artist, educator, and entrepreneur has provided me excellent problem-solving, time management, and communication skills. I spent most of my life explaining concepts in anologies becuase what I was talking about wasn't tangible. I believe that curiosity, drive, and passion are an excellent and almost dangerous combination for an emerging developer - I'm excited to see what happens.
           </p>
           <p>Feel free to reach out to me on <span/>
             <a className='bio-links' href="https://www.linkedin.com/in/davidleach724/">linkedIn</a>, <span/>
