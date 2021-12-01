e3ae98c9d4c517cd5b8fa057101027e128ce79a0
David Leach <davidleach724@gmail.com>
2021-11-30 17:56:11 -0700

    Prettier Projects component

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,22 +1,18 @@
-import ProjectCard from '../ProjectCard/ProjectCard'
-import { projectdata } from './projectdata'
-import './Projects.css'
+import ProjectCard from '../ProjectCard/ProjectCard';
+import { projectdata } from './projectdata';
+import './Projects.css';
 
 const Projects = () => {
-  const projectCards = projectdata.map(project => {
-    return (
-      <ProjectCard project={ project } key = { project.id }/>
-    )
-  })
+  const projectCards = projectdata.map((project) => {
+    return <ProjectCard project={project} key={project.id} />;
+  });
 
   return (
     <div className="projects-container">
       <h2 className="skills-title">Projects</h2>
-      <div className="project-card-container">
-        {projectCards}
-      </div>
+      <div className="project-card-container">{projectCards}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
