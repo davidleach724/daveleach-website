fc3085cefbdad35ebe1d257d5f7c3938535faa38
David Leach <davidleach724@gmail.com>
2021-11-30 17:35:21 -0700

    Prettier home js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,10 @@
-import './Home.css';
-
-import city from '../../Images/smallcity.png'
-import tree from '../../Images/smalltree.png'
-import moon from '../../Images/smallmoon.png'
-
 import Particles from 'react-tsparticles';
 import tsParticleConfig from '../config/tsParticleConfig';
 import { useEffect, useState } from 'react';
+import city from '../../Images/smallcity.png';
+import moon from '../../Images/smallmoon.png';
+import tree from '../../Images/smalltree.png';
+import './Home.css';
 
 const Home = () => {
   const [currentScrollHeight, setCurrentScrollHeight] = useState(0);
@@ -29,10 +27,10 @@ const Home = () => {
         <h1 className="name bounce-in-top">DAVE LEACH</h1>
         <h2 className="occupation text-focus-in">Software Engineer</h2>
       </div>
-      <img src={ moon } className='moon' alt='moon sketch'/>
-      <img src={ city } className='city' alt='city sketch'/>
-      <div className='background-bottom'>
-        <img src={ tree } className='tree' alt='tree sketch'/>
+      <img src={moon} className="moon" alt="moon sketch" />
+      <img src={city} className="city" alt="city sketch" />
+      <div className="background-bottom">
+        <img src={tree} className="tree" alt="tree sketch" />
       </div>
       <div className="firefly"></div>
       <div className="firefly"></div>
