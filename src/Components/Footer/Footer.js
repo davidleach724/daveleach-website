b34b96f9a9ed4352d62524b1335b7a8d04aca872
David Leach <davidleach724@gmail.com>
2021-11-30 17:57:20 -0700

    Prettier footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,34 +1,33 @@
-import github from '../../Images/github.svg'
-import resume from '../../Images/LeachResume.pdf'
-import linkedin from '../../Images/linkedin-logo.svg'
-import resumeLogo from '../../Images/resume-download.png'
-import email from '../../Images/email-open.svg'
-import './Footer.css'
+import github from '../../Images/github.svg';
+import resume from '../../Images/LeachResume.pdf';
+import linkedin from '../../Images/linkedin-logo.svg';
+import resumeLogo from '../../Images/resume-download.png';
+import email from '../../Images/email-open.svg';
+import './Footer.css';
 
 const Footer = () => {
-
   return (
     <div className="footer-container">
       <div className="footer-copy"></div>
       <div className="footer-links">
-        <a href="https://github.com/davidleach724" >
-          <img className="footer-icon" src={github} alt="github logo"/>
+        <a href="https://github.com/davidleach724">
+          <img className="footer-icon" src={github} alt="github logo" />
         </a>
-        <a href="https://www.linkedin.com/in/davidleach724/" >
-          <img className="footer-icon" src={linkedin} alt="linkedIn logo"/>
+        <a href="https://www.linkedin.com/in/davidleach724/">
+          <img className="footer-icon" src={linkedin} alt="linkedIn logo" />
         </a>
-        <a href={"mailto:davidleach724@gmail.com"} >
-          <img className="footer-icon" src={email} alt="email logo"/>
+        <a href={'mailto:davidleach724@gmail.com'}>
+          <img className="footer-icon" src={email} alt="email logo" />
         </a>
-        <a href={resume} >
-          <img className="footer-icon" src={resumeLogo} alt="resume logo"/>
+        <a href={resume}>
+          <img className="footer-icon" src={resumeLogo} alt="resume logo" />
         </a>
       </div>
       <div className="footer-copy">
         <h4>© Dave Leach 2021</h4>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
